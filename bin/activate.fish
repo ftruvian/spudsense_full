069348034d2157fe24094a8716d461f35c1f436c
Joe <joe@debian>
2025-10-13 09:14:48 +0800

    stuff2

--- bin/activate.fish
+++ bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /mnt/1tb/joe/projects/spudsense_full
+set -gx VIRTUAL_ENV /home/joe/git/spudsense_full
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
